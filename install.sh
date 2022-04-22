726ea433ec2de9c271b94b2b41797889fd02fd82
Mattia Procopio <promat85@gmail.com>
2022-04-22 15:35:03 +0200

    Add bash installer

---
+++ install.sh
@@ -0,0 +1,32 @@
+OS=$(uname -s)
+MACHINE=$(uname -m)
+VERSION=$(curl -s https://api.github.com/repos/mattblack85/rnav-alerts/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
+PACKAGE_NAME=rnav-alerts
+EXTENSION=tar.gz
+
+case $OS in
+    Linux)
+	case $MACHINE in
+	    x86_64)
+		ARCH=linux-x64
+		;;
+	    armv7l)
+		ARCH=armv7
+		;;
+	    aarch64)
+		ARCH=arm64
+	esac
+	;;
+    Darwin)
+	ARCH=macos
+	;;
+    *)
+	echo "OS not supported, please raise an issue at https://github.com/MattBlack85/rnav-alerts/issues giving the result of `uname -s`"
+	;;
+esac
+
+
+wget https://github.com/MattBlack85/rnav-alerts/releases/download/$VERSION/$PACKAGE_NAME-$ARCH-$VERSION.$EXTENSION
+tar -xvzf rnav-alerts*.tar.gz
+rm rnavalerts*tar.gz
+sudo mv rnav-alerts /usr/local/bin/
